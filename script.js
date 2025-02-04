0abd5e7e7cbaa39ad7cee4c513dc7d4bf3310dac
aladin-97 <fadhlialadin8@gmail.com>
2025-02-04 11:27:41 +0100

    td-2

---
+++ script.js
@@ -0,0 +1,13 @@
+let x 
+do {
+x = prompt('donnez valeur 1')
+if (x > 20 ) {
+    alert('plus grande')
+}
+else if (x < 10){
+    alert('plus petit')
+}
+
+}
+while (x >= 20 || x <= 10)
+    alert('perfect')
